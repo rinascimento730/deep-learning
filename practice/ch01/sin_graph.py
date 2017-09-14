414f8e2e4cee52ffda640edb6c9b9c22b158182b
Satoshi Fujinaga <fujinaga@gnavi.co.jp>
2017-09-14 15:49:27 +0900

    ch1練習ファイル

---
+++ practice/ch01/sin_graph.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+import numpy as np
+import matplotlib.pyplot as plt
+
+# データの作成
+x = np.arange(0, 6, 0.1)
+y = np.sin(x)
+
+# グラフの描画
+plt.plot(x, y)
+#plt.show()
+filename = "output/sin_graph.png"
+plt.savefig(filename)
